3bb89212e3b87f6329ad496b802c1acb6332c25d
Guillem Jover <guillem@hadrons.org>
2026-03-19 23:09:52 +0100

    Add internal explicit_bzero() support
    This imports the code from libbsd, to avoid a dependency loop.

---
+++ src/bzero.c
@@ -0,0 +1,50 @@
+/*
+ * Copyright © 2026 Guillem Jover <guillem@hadrons.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <string.h>
+
+#if !defined HAVE_MEMSET_EXPLICIT && !defined HAVE_EXPLICIT_MEMSET
+__attribute__((__weak__)) void
+__explicit_bzero_hook(void *, size_t);
+
+__attribute__((__weak__)) void
+__explicit_bzero_hook(void *buf, size_t len)
+{
+}
+#endif
+
+void *
+explicit_bzero(void *buf, size_t len)
+{
+#ifdef HAVE_MEMSET_EXPLICIT
+	memset_explicit(buf, 0, len);
+#elif HAVE_EXPLICIT_MEMSET
+	explicit_memset(buf, 0, len);
+#else
+	memset(buf, 0, len);
+	__explicit_bzero_hook(buf, len);
+#endif
+}
